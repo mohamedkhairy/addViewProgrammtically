98db3808aa4a7ee76517cd6c739e401121035b07
mohamedkhairy <memo2030400@gmail.com>
2018-12-30 12:33:35 +0200

    new merge

--- app/src/main/java/khairy/com/addview/ViewActivity.kt
+++ app/src/main/java/khairy/com/addview/ViewActivity.kt
@@ -8,5 +8,7 @@ class ViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view)
+
+        var test = testClass("khairy")
     }
 }
--- app/src/main/java/khairy/com/addview/testClass.java
+++ app/src/main/java/khairy/com/addview/testClass.java
@@ -1,4 +1,8 @@
 package khairy.com.addview;
 
 public class testClass {
+
+    public testClass(String n){
+        String name = n;
+    }
 }
